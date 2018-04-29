16642c2f09409dff61183fc7eae49a0a12751687
m.daghestani <m.daghestani@asanpardakht.ir>
2018-04-29 17:56:13 +0430

    adding Microsoft.Owin for test

--- MRD.OwinMiddlewares/Class1.cs
+++ MRD.OwinMiddlewares/Class1.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin;
 
 namespace MRD.OwinMiddlewares
 {
-    public class Class1
+    public class Class1 : OwinMiddleware
     {
+        protected Class1(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
